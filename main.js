e1fa25d478b268b6d5e018de2a03fddb9a855988
MITHUN G P <mithungpgowda549@gmail.com>
2024-12-23 13:13:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+const options = {
+    strings: ["Developer", "Designer", "Programmer"], // List of words you want to type
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1000,
+    startDelay: 500,
+    loop: true
+};
+new Typed(".text", options);
